bff1c62a8250d5776cf5f98481b0d1ced76469df
yuyang <eryuyang@gmail.com>
2025-09-21 01:38:54 +0800

    replace url with downloaded images

--- components/MapWithPins.jsx
+++ components/MapWithPins.jsx
@@ -8,8 +8,8 @@ const MapWithPins = ( {centerCoordinate, zoomValue, locations} ) => {
 
   // Default blue icon (same as Leaflet's default)
   const defaultIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconUrl: '../images/marker-icon.png',
+    shadowUrl: '../images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
@@ -18,8 +18,8 @@ const MapWithPins = ( {centerCoordinate, zoomValue, locations} ) => {
 
   // Red icon
   const redIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconUrl: '../images/marker-icon-red.png',
+    shadowUrl: '../images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
